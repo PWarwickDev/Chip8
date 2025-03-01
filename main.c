5eda649eeb8f4bb4b9da0837cb6a83e531349bb9
PwarwickDev <pwarwick272@gmail.com>
2025-03-01 00:00:18 +0000

    change functions to pass by reference

--- main.c
+++ main.c
@@ -35,124 +35,124 @@ typedef struct context
 } Context;
 
 // function to place pre-designed fonts into memory from 050 - 09F (80 - 159 in decimal)
-void setup_fonts(Context context)
+void setup_fonts(Context *context)
 {
   // font of 0
-  context.memory[0x050] = 0xF0;
-  context.memory[0x051] = 0x90;
-  context.memory[0x052] = 0x90;
-  context.memory[0x053] = 0x90;
-  context.memory[0x054] = 0xF0;
+  context->memory[0x050] = 0xF0;
+  context->memory[0x051] = 0x90;
+  context->memory[0x052] = 0x90;
+  context->memory[0x053] = 0x90;
+  context->memory[0x054] = 0xF0;
 
   // font of 1
-  context.memory[0x055] = 0x20;
-  context.memory[0x056] = 0x60;
-  context.memory[0x057] = 0x20;
-  context.memory[0x058] = 0x20;
-  context.memory[0x059] = 0x70;
+  context->memory[0x055] = 0x20;
+  context->memory[0x056] = 0x60;
+  context->memory[0x057] = 0x20;
+  context->memory[0x058] = 0x20;
+  context->memory[0x059] = 0x70;
 
   // font of 2
-  context.memory[0x05A] = 0xF0;
-  context.memory[0x05B] = 0x10;
-  context.memory[0x05C] = 0xF0;
-  context.memory[0x05D] = 0x80;
-  context.memory[0x05E] = 0xF0;
+  context->memory[0x05A] = 0xF0;
+  context->memory[0x05B] = 0x10;
+  context->memory[0x05C] = 0xF0;
+  context->memory[0x05D] = 0x80;
+  context->memory[0x05E] = 0xF0;
 
   // font of 3
-  context.memory[0x05F] = 0xF0;
-  context.memory[0x060] = 0x10;
-  context.memory[0x061] = 0xF0;
-  context.memory[0x062] = 0x10;
-  context.memory[0x063] = 0xF0;
+  context->memory[0x05F] = 0xF0;
+  context->memory[0x060] = 0x10;
+  context->memory[0x061] = 0xF0;
+  context->memory[0x062] = 0x10;
+  context->memory[0x063] = 0xF0;
 
   // font of 4
-  context.memory[0x064] = 0x90;
-  context.memory[0x065] = 0x90;
-  context.memory[0x066] = 0xF0;
-  context.memory[0x067] = 0x10;
-  context.memory[0x068] = 0x10;
+  context->memory[0x064] = 0x90;
+  context->memory[0x065] = 0x90;
+  context->memory[0x066] = 0xF0;
+  context->memory[0x067] = 0x10;
+  context->memory[0x068] = 0x10;
 
   // font of 5
-  context.memory[0x069] = 0xF0;
-  context.memory[0x06A] = 0x80;
-  context.memory[0x06B] = 0xF0;
-  context.memory[0x06C] = 0x10;
-  context.memory[0x06D] = 0xF0;
+  context->memory[0x069] = 0xF0;
+  context->memory[0x06A] = 0x80;
+  context->memory[0x06B] = 0xF0;
+  context->memory[0x06C] = 0x10;
+  context->memory[0x06D] = 0xF0;
 
   // font of 6
-  context.memory[0x06E] = 0xF0;
-  context.memory[0x06F] = 0x80;
-  context.memory[0x070] = 0xF0;
-  context.memory[0x071] = 0x90;
-  context.memory[0x072] = 0xF0;
+  context->memory[0x06E] = 0xF0;
+  context->memory[0x06F] = 0x80;
+  context->memory[0x070] = 0xF0;
+  context->memory[0x071] = 0x90;
+  context->memory[0x072] = 0xF0;
 
   // font of 7
-  context.memory[0x073] = 0xF0;
-  context.memory[0x074] = 0x10;
-  context.memory[0x075] = 0x20;
-  context.memory[0x076] = 0x40;
-  context.memory[0x077] = 0x40;
+  context->memory[0x073] = 0xF0;
+  context->memory[0x074] = 0x10;
+  context->memory[0x075] = 0x20;
+  context->memory[0x076] = 0x40;
+  context->memory[0x077] = 0x40;
 
   // font of 8
-  context.memory[0x078] = 0xF0;
-  context.memory[0x079] = 0x90;
-  context.memory[0x07A] = 0xF0;
-  context.memory[0x07B] = 0x90;
-  context.memory[0x07C] = 0xF0;
+  context->memory[0x078] = 0xF0;
+  context->memory[0x079] = 0x90;
+  context->memory[0x07A] = 0xF0;
+  context->memory[0x07B] = 0x90;
+  context->memory[0x07C] = 0xF0;
 
   // font of 9
-  context.memory[0x07D] = 0xF0;
-  context.memory[0x07E] = 0x90;
-  context.memory[0x07F] = 0xF0;
-  context.memory[0x080] = 0x10;
-  context.memory[0x081] = 0xF0;
+  context->memory[0x07D] = 0xF0;
+  context->memory[0x07E] = 0x90;
+  context->memory[0x07F] = 0xF0;
+  context->memory[0x080] = 0x10;
+  context->memory[0x081] = 0xF0;
 
   // font of A
-  context.memory[0x082] = 0xF0;
-  context.memory[0x083] = 0x90;
-  context.memory[0x084] = 0xF0;
-  context.memory[0x085] = 0x90;
-  context.memory[0x086]= 0x90;
+  context->memory[0x082] = 0xF0;
+  context->memory[0x083] = 0x90;
+  context->memory[0x084] = 0xF0;
+  context->memory[0x085] = 0x90;
+  context->memory[0x086]= 0x90;
 
   // font of B
-  context.memory[0x087] = 0xE0;
-  context.memory[0x088] = 0x90;
-  context.memory[0x089] = 0xE0;
-  context.memory[0x08A] = 0x90;
-  context.memory[0x08B] = 0xE0;
+  context->memory[0x087] = 0xE0;
+  context->memory[0x088] = 0x90;
+  context->memory[0x089] = 0xE0;
+  context->memory[0x08A] = 0x90;
+  context->memory[0x08B] = 0xE0;
 
   // font of C
-  context.memory[0x08C] = 0xF0;
-  context.memory[0x08D] = 0x80;
-  context.memory[0x08E] = 0x80;
-  context.memory[0x08F] = 0x80;
-  context.memory[0x090] = 0xF0;
+  context->memory[0x08C] = 0xF0;
+  context->memory[0x08D] = 0x80;
+  context->memory[0x08E] = 0x80;
+  context->memory[0x08F] = 0x80;
+  context->memory[0x090] = 0xF0;
 
   // font of D
-  context.memory[0x091] = 0xE0;
-  context.memory[0x092] = 0x90;
-  context.memory[0x093] = 0x90;
-  context.memory[0x094] = 0x90;
-  context.memory[0x095] = 0xE0;
+  context->memory[0x091] = 0xE0;
+  context->memory[0x092] = 0x90;
+  context->memory[0x093] = 0x90;
+  context->memory[0x094] = 0x90;
+  context->memory[0x095] = 0xE0;
 
   // font of E
-  context.memory[0x096] = 0xF0;
-  context.memory[0x097] = 0x80;
-  context.memory[0x098] = 0xF0;
-  context.memory[0x099] = 0x80;
-  context.memory[0x09A] = 0xF0;
+  context->memory[0x096] = 0xF0;
+  context->memory[0x097] = 0x80;
+  context->memory[0x098] = 0xF0;
+  context->memory[0x099] = 0x80;
+  context->memory[0x09A] = 0xF0;
 
   // font of F
-  context.memory[0x09B] = 0xF0;
-  context.memory[0x09C] = 0x80;
-  context.memory[0x09D] = 0xF0;
-  context.memory[0x09E] = 0x80;
-  context.memory[0x09F] = 0x80;
+  context->memory[0x09B] = 0xF0;
+  context->memory[0x09C] = 0x80;
+  context->memory[0x09D] = 0xF0;
+  context->memory[0x09E] = 0x80;
+  context->memory[0x09F] = 0x80;
 
 }
 
 // Add target program into interpreter memory
-int loadProgram(char* fileName, Context context) {
+int loadProgram(char *fileName, Context *context) {
   FILE *fp = fopen(fileName, "r");
 
   // Scan file contents into memory byte by byte
@@ -162,9 +162,9 @@ int loadProgram(char* fileName, Context context) {
   } else {
     char c;
     // start loading at 0x200, but don't modify PC
-    int i = context.PC;
+    int i = context->PC;
     while (fscanf(fp, "%c", &c) != EOF) {
-      context.memory[i] = c;
+      context->memory[i] = c;
       i++;
     }
   }
@@ -172,6 +172,22 @@ int loadProgram(char* fileName, Context context) {
   fclose(fp);
 
   return 0;
+}
+
+// takes 2 bytes from memory, combines them to make a decodable instruction.
+unsigned short fetch(Context *context) {
+  // Program conclusion
+  if (&context->memory[context->PC] == NULL)
+    return -1;
+
+  unsigned short instruction;
+
+  // some bitshifting to combine 2 bytes into 1 instruction
+  instruction = (context->memory[context->PC] << 8) + context->memory[context->PC + 1]; 
+
+  context->PC += 2;
+  
+  return instruction;
 }
 		    
 
@@ -188,10 +204,10 @@ int main(int argc, char* argv[]) {
   context.PC = 0x200;
 
   // load fonts in
-  setup_fonts(context);
+  setup_fonts(&context);
 
   // load target program into memory
-  if (loadProgram(argv[1], context) != 0) {
+  if (loadProgram(argv[1], &context) != 0) {
     // fail early due to file error
     return -1;
   }
@@ -231,6 +247,13 @@ int main(int argc, char* argv[]) {
 	while (SDL_PollEvent(&e)) {
 	  if (e.type  == SDL_QUIT)
 	    quit = 1;
+
+	  // main emulation cycle
+
+	  unsigned short instruction = fetch(&context);
+	  printf("PC: %d\n", context.PC);
+	  
+	  
 	}
       }
     }
